6ed576470e1f56247bbefde56e50b7e6115d6181
Emmanuel Gabriel <Emmanueltijesu@gmail.com>
2024-10-24 02:58:29 +0100

    Delete src directory

--- src/api/endPoints.js
+++
@@ -1,7 +0,0 @@
-const endpoints = {
-    getStates: "/v0.1/countries/states/",
-    getCities: "/v0.1/countries/state/cities/",
-    getPosts: "/v0.1/countries/states/q"  /* creatung endpoint*/
-  };
-  
-  export default endpoints;
